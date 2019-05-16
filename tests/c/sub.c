1069fb644b6396b002b6d2785b6a1d29ac81fd3e
Baptiste PRUNIER <klevh1@gmail.com>
2019-05-16 20:43:23 +0200

    Update sub.c

--- tests/c/sub.c
+++ tests/c/sub.c
@@ -1,6 +1,6 @@
+#if defined(unix) || defined(__unix) || defined(__unix__) || defined(__APPLE__) || defined(__MACH__) || defined(__GNUC__)
 #include "../../LAssert.h"
 
-#ifndef LASSERT_WINDOWS
 TEST_SECTION(pouloulou){
     puts("2");
 }
